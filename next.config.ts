aac409c2c88a2fa19efce27f4051354aab9f9512
hermantia130804-ctrl <hermantia130804-ctrl@users.noreply.github.com>
2026-09-22 07:00:13 +0700

    skip eslint saat build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // ...isi lain biarkan tetap ada
 };
-
-export default nextConfig;
